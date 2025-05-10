8fe86ef72f61611dc816aa8201218cf90e36b662
edgar_harutyunyan_2025 <edgar_harutyunyan_2025@mail.ru>
2025-05-10 18:57:46 +0400

    fix

--- packer_image/build_ami_version.sh
+++ packer_image/build_ami_version.sh
@@ -5,14 +5,11 @@ TF_DIR="../terraform"
 AMI_ID=$(terraform -chdir="$TF_DIR" output -raw ami_latest 2>/dev/null)
 
 
-VERSION_FILE="version.txt"
-
-if [ ! -f "$VERSION_FILE" ]; then
-  echo "0" > "$VERSION_FILE"
+if [ -n "$GITHUB_SHA" ]; then
+  NEW_VERSION="${GITHUB_SHA:0:7}"
+else
+  NEW_VERSION="local-build"
 fi
-
-VERSION=$(cat "$VERSION_FILE")
-NEW_VERSION=$((VERSION + 1))
 
 
 if [[ "$AMI_ID" =~ ^ami- ]]; then	
